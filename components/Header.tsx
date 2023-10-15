54f49965743b6ac6a6b2d2f24a73b53414f8e604
Sol Lee <sol@Sols-MacBook-Pro.local>
2023-10-15 20:22:19 +0900

    fixing link redundancy issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,32 +26,31 @@ function Header({}: Props) {
             />
          </MotionDiv>
 
-         <Link href="#contact">
-            <MotionDiv
-               initial={{
-                  x: 500,
-                  opacity: 0,
-                  scale: 0.5,
-               }}
-               animate={{
-                  x: 0,
-                  opacity: 1,
-                  scale: 1,
-               }}
-               transition={{ duration: 1.5 }}
-               className="flex flex-row items-center text-gray-300 cursor-pointer"
-            >
-               <SocialIcon
-                  className="cursor-pointer"
-                  network="email"
-                  fgColor="gray"
-                  bgColor="transparent"
-               />
-               <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-                  Get in touch
-               </p>
-            </MotionDiv>
-         </Link>
+         <MotionDiv
+            initial={{
+               x: 500,
+               opacity: 0,
+               scale: 0.5,
+            }}
+            animate={{
+               x: 0,
+               opacity: 1,
+               scale: 1,
+            }}
+            transition={{ duration: 1.5 }}
+            className="flex flex-row items-center text-gray-300 cursor-pointer"
+         >
+            <SocialIcon
+               href="#contact"
+               className="cursor-pointer"
+               network="email"
+               fgColor="gray"
+               bgColor="transparent"
+            />
+            <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+               Get in touch
+            </p>
+         </MotionDiv>
       </header>
    );
 }
