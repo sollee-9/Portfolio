64eb70e10ca494efa33769013b776ab899f7c365
Sol Lee <sol@Sols-MacBook-Pro.local>
2024-01-05 17:39:25 -0800

    about section refactored

--- components/About.tsx
+++ components/About.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import profile from "../images/profile.jpeg";
+import { about } from "../data/About";
 
 type Props = {};
 
@@ -40,35 +41,7 @@ function About({}: Props) {
                className="max-h-[180px] w-full sm:ml-4 sm:max-h-[400px] mt-3 px-8 text-sm sm:text-[15px] 
                text-gray-500 text-left whitespace-pre-line text-ellipsis overflow-y-scroll lg:scrollbar-hide"
             >
-               Hey, there! 👋🏼 I&apos;m Sol, a tech enthusiast on a
-               mission to bridge the worlds of business strategy and
-               cutting-edge technology
-               {"\n"}
-               {"\n"}
-               🎓 I started my university journey at Cornell
-               University&apos;s SC Johnson College of Business before
-               pivoting to immerse myself in the dynamic realm of
-               Computer Science at The University of British Columbia
-               {"\n"}
-               {"\n"}
-               🚀 Throughout my academic and professional journey, I
-               had the privilege of diving into all realms of
-               “product”: from product design, product management to
-               software engineering, which gave me a holistic
-               understanding of the entire product development
-               lifecycle. I learned that my true passion lies in
-               crafting innovative solutions and transforming ideas
-               into successful products
-               {"\n"}
-               {"\n"}
-               🌐 What drew me to the tech space is the endless
-               opportunities for learning, the constant evolution of
-               technology, and the joy of creating products that
-               people love. From engineering to user experience,
-               I&apos;m passionate about making an impact and
-               fostering innovation through thoughtful design and
-               strategic thinking to transform ideas into impactful
-               solutions
+               {about}
             </p>
          </div>
       </MotionDiv>
