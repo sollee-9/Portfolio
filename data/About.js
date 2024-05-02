5303eb364f66a98aebc171e5deab77f27a462b44
Sol Lee <sol@Sols-MacBook-Pro.local>
2024-05-02 00:53:17 -0700

    portfolio update

--- data/About.js
+++ data/About.js
@@ -1,8 +1,8 @@
-export const about = `Hey, there! 👋🏼 I'm Sol, a tech enthusiast on a mission to bridge the worlds of business and technology
+export const about = `Hi, there! 👋🏼 I'm Sol, a Product Manager and tech enthusiast
 
 🎓 I started my academic journey at Cornell University's SC Johnson College of Business before pivoting to the dynamic field of Computer Science at The University of British Columbia
 
-🚀 Throughout my academic and professional journey, I had the privilege of diving into all areas of “product”: from product design, product management to software engineering, which gave me a holistic understanding of the entire product development lifecycle. I learned that my true passion lies in crafting innovative solutions and transforming ideas into successful products
+🚀 Throughout my academic and professional journey, I had the privilege of diving into all areas of “product”: from product design, product management to software engineering, that gave me a holistic understanding of the entire product development lifecycle
 
-🌐 What drew me to the tech space is the endless opportunities for learning, the constant evolution of technology, and the joy of creating products that people love. From engineering to user experience, I'm passionate about making an impact and fostering innovation through thoughtful design and strategic thinking to transform ideas into impactful solutions
+🌐  I learned that my passion lies in crafting innovative solutions and transforming ideas into successful products that delight users through thoughtful, user-centric design methods and strategic thinking
 `;
--- data/Projects.js
+++ data/Projects.js
@@ -4,7 +4,7 @@ import airbnb from "../images/projects/airbnb.png";
 import connexa from "../images/projects/connexa.png";
 import portfolio from "../images/projects/portfolio.png";
 import notion from "../images/projects/notion.png";
-
+import wonder from "@/images/projects/wonder.png";
 // skills
 import javascript from "../images/skills/javascript.png";
 import node from "../images/skills/node.png";
@@ -24,78 +24,99 @@ import tailwind from "../images/skills/tailwind.png";
 import ts from "../images/skills/ts.png";
 
 export const projects = [
-   {
-      title: "Notion - Collaborative Workspace",
-      shortName: "notion",
-      img: notion,
-      skills: [{ title: "Figma", img: figma }],
-      role: "Product Manager, UX Researcher & Product Designer",
-      description: `This project aims to resolve the perceived scalability challenges faced by Notion users and introduces enhancements to foster a collaborative environment tailored to growing teams 
+  {
+    title: "Wonder AI",
+    shortName: "wonder",
+    img: wonder,
+    skills: [
+      { title: "Figma", img: figma },
+      { title: "React.js", img: react },
+      { title: "Next.js", img: next },
+      { title: "Node.js", img: node },
+      { title: "Express.js", img: express },
+      { title: "AWS", img: aws },
+      { title: "Git", img: git },
+    ],
+    role: "Founder & Head of Product",
+    description: `Wonder redefines management in the creator economy with an all-in-one platform that integrates project management, AI-driven communication tools, and data analytics
+    
+          As a founder, I spearheaded product development & strategy with particular focus on core product and led a multidisciplinary team of engineering, design and marketing
+        `,
+    url: "https://www.wonder-ai.co/",
+    buttonOption: "Learn more",
+  },
+  {
+    title: "Notion - Collaborative Workspace",
+    shortName: "notion",
+    img: notion,
+    skills: [{ title: "Figma", img: figma }],
+    role: "Product Manager, UX Researcher & Product Designer",
+    description: `This project aims to resolve the perceived scalability challenges faced by Notion users and introduces enhancements to foster a collaborative environment tailored to growing teams 
       
       Key features include centralized comments, flexible drawing board for brainstorming & ideation, easy-to-use visualization tool that integrates seamlessly with their database systems 
       `,
-      url: "/projects/notion",
-      buttonOption: "Learn more",
-   },
-   {
-      title: "Airbnb Compare & Suggest",
-      shortName: "airbnb",
-      img: airbnb2,
-      role: "Product Manager, Software Engineer & Product Designer",
+    url: "/projects/notion",
+    buttonOption: "Learn more",
+  },
+  {
+    title: "Airbnb Compare & Suggest",
+    shortName: "airbnb",
+    img: airbnb2,
+    role: "Product Manager, Software Engineer & Product Designer",
 
-      skills: [
-         { title: "JavaScript", img: javascript },
-         { title: "Next.js", img: next },
-         { title: "React.js", img: react },
-         { title: "Tailwind", img: tailwind },
-         { title: "Figma", img: figma },
-         { title: "Git", img: git },
-      ],
-      description: `With Compare & Suggest, users can effortlessly compare wishlist properties, receive personalized recommendations, and benefit from AI-generated summaries of guest reviews
+    skills: [
+      { title: "JavaScript", img: javascript },
+      { title: "Next.js", img: next },
+      { title: "React.js", img: react },
+      { title: "Tailwind", img: tailwind },
+      { title: "Figma", img: figma },
+      { title: "Git", img: git },
+    ],
+    description: `With Compare & Suggest, users can effortlessly compare wishlist properties, receive personalized recommendations, and benefit from AI-generated summaries of guest reviews
       
       This solution empowers users with efficient decision-making tools, transforming the Airbnb experience by streamlining property selection and enabling efficient travel planning on Airbnb
       `,
-      url: "/projects/airbnb",
-      buttonOption: "Learn more",
-   },
-   {
-      title: "Connexa.ai",
-      shortName: "connexa",
-      img: connexa,
-      role: "Product Manager & Software Engineer",
+    url: "/projects/airbnb",
+    buttonOption: "Learn more",
+  },
+  {
+    title: "Connexa.ai",
+    shortName: "connexa",
+    img: connexa,
+    role: "Product Manager & Software Engineer",
 
-      skills: [
-         { title: "JavaScript", img: javascript },
-         { title: "React.js", img: react },
-         { title: "Figma", img: figma },
-         { title: "AWS", img: aws },
-         { title: "Git", img: git },
-      ],
-      description: `Connexa.ai is transforming customer service for small to medium businesses (SMBs) with a mission to facilitate genuine human connections
+    skills: [
+      { title: "JavaScript", img: javascript },
+      { title: "React.js", img: react },
+      { title: "Figma", img: figma },
+      { title: "AWS", img: aws },
+      { title: "Git", img: git },
+    ],
+    description: `Connexa.ai is transforming customer service for small to medium businesses (SMBs) with a mission to facilitate genuine human connections
 
       Tackling the challenge of inconsistent customer experiences across channels, Connexa.ai introduces an all-in-one inbox with AI-powered analytics dashboard for real-time insights`,
 
-      url: "https://www.connexa.ai/",
-      buttonOption: "Learn more",
-   },
-   {
-      title: "Portfolio",
-      shortName: "portfolio",
-      img: portfolio,
-      role: "Software Engineer & Designer",
+    url: "https://www.connexa.ai/",
+    buttonOption: "Learn more",
+  },
+  {
+    title: "Portfolio",
+    shortName: "portfolio",
+    img: portfolio,
+    role: "Software Engineer & Designer",
 
-      skills: [
-         { title: "TypeScript", img: ts },
-         { title: "Next.js", img: next },
-         { title: "React.js", img: react },
-         { title: "Tailwind", img: tailwind },
-         { title: "Figma", img: figma },
-         { title: "Git", img: git },
-      ],
-      description:
-         "This very website that you are viewing right now was designed on Figma and built using TypeScript, Next, React, and Tailwind CSS \n \n It features a modern website design with various dynamic interactive features and animations throughout the pages for an immersive user experience ",
+    skills: [
+      { title: "TypeScript", img: ts },
+      { title: "Next.js", img: next },
+      { title: "React.js", img: react },
+      { title: "Tailwind", img: tailwind },
+      { title: "Figma", img: figma },
+      { title: "Git", img: git },
+    ],
+    description:
+      "This very website that you are viewing right now was designed on Figma and built using TypeScript, Next, React, and Tailwind CSS \n \n It features a modern website design with various dynamic interactive features and animations throughout the pages for an immersive user experience ",
 
-      url: "https://github.com/sollee-9/Portfolio",
-      buttonOption: "Learn more",
-   },
+    url: "https://github.com/sollee-9/Portfolio",
+    buttonOption: "Learn more",
+  },
 ];
